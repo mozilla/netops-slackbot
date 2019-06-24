df4c97c8e7e3efe56c2e709a38155646f429759b
Dave Miller <justdave@mozilla.com>
2019-06-24 15:18:28 -0400

    Ported to slackclient 2.x
    Additional changes:
    - bot now responds to "!oncall" instead of needing to prefix it with the bot name

--- netops-slackbot.py
+++ netops-slackbot.py
@@ -1,43 +1,25 @@
 #!/usr/bin/env python
 import os
-import time
-import re
-from slackclient import SlackClient
 import yaml
 import json
 import requests
-
-with open("config.yml", 'r') as ymlfile:
-    cfg = yaml.safe_load(ymlfile)
-
-# instantiate Slack client
-slack_client = SlackClient(cfg['slack_api_token'])
-
-# the bot's user ID in Slack: value is assigned after the bot starts up
-bot_id = None
-
-# oncall person object
-oncall = cfg["default_oncall"]
-channels = {}
+import slack
+import ssl as ssl_lib
+import certifi
+import asyncio
+import nest_asyncio
 
 # constants
 RECONNECT_DELAY = 30 # how long to wait between retries when the connection fails
-RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
-MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
-DEFAULT_CHANNEL = cfg["oncall_announce_channel"]
-DEBUG = 0
-
-def get_channels():
-    channel_list = slack_client.api_call("users.conversations", types="public_channel,private_channel")
-    if DEBUG:
-        print("Fetching users.conversations:")
-        print(json.dumps(channel_list, indent=4))
-    global channels
-    channels = {}
-    for channelobj in channel_list['channels']:
-        channels[channelobj['name']] = channelobj
+PREFIX_CHAR = "!"
+DEBUG = 1
+polling_initialized = 0
+nest_asyncio.apply()
 
 def get_oncall():
+    """Make an API request to PagerDuty to fetch the current oncall person.
+       Just updates the in-memory data structure.
+    """
     global oncall
     headers = {
         'Authorization': 'Token token={0}'.format(cfg["pagerduty_api_token"]),
@@ -72,32 +54,9 @@ def get_oncall():
     except Exception as e:
         print("Got an error looking up the oncall in PagerDuty: %s" % e)
         raise
-
     return
 
-def parse_bot_commands(slack_events):
-    """
-        Parses a list of events coming from the Slack RTM API to find bot commands.
-        If a bot command is found, this function returns a tuple of command and channel.
-        If its not found, then this function returns None, None.
-    """
-    for event in slack_events:
-        if event["type"] == "message" and not "subtype" in event:
-            user_id, message = parse_direct_mention(event["text"])
-            if user_id == bot_id:
-                return message, event["channel"]
-    return None, None
-
-def parse_direct_mention(message_text):
-    """
-        Finds a direct mention (a mention that is at the beginning) in message text
-        and returns the user ID which was mentioned. If there is no direct mention, returns None
-    """
-    matches = re.search(MENTION_REGEX, message_text)
-    # the first group contains the username, the second group contains the remaining message
-    return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
-
-def post_current_oncall(channel, pretext="The current oncall network engineer is:"):
+def post_current_oncall(web_client, channel, pretext="The current oncall network engineer is:"):
     """
         Posts the current oncall information to the given channel
     """
@@ -124,96 +83,100 @@ def post_current_oncall(channel, pretext="The current oncall network engineer is
         "thumb_url": oncall["avatar_url"],
         "footer": "Oncall from {0} to {1}.".format(oncall["start"],oncall["end"])
     }])
-    channel_info = slack_client.api_call(
-            "conversations.info",
-            channel = channel)
-    if not channel_info["ok"]:
-        print("Looking up channel: %s" % channel)
-        print("Got an error from conversations.info: %s" % channel_info["error"])
-        print(json.dumps(channel_info))
-    else:
-        print("Posting current oncall (%s) to #%s" % (oncall["email"], channel_info["channel"]["name"]))
-        slack_client.api_call(
-                "chat.postMessage",
-                channel = channel,
-                attachments = attachments)
-        #slack_client.api_call(
-        #        "conversations.setTopic",
-        #        channel = channel,
-        #        topic = "Current NetOps oncall is %s" % oncall["slack_nick"])
+    print("Posting current oncall (%s) to #%s" % (oncall["email"], channel))
+    web_client.chat_postMessage(
+            channel = channel,
+            attachments = attachments)
+    #slack_client.api_call(
+    #        "conversations.setTopic",
+    #        channel = channel,
+    #        topic = "Current NetOps oncall is %s" % oncall["slack_nick"])
     return
 
-def handle_command(command, channel):
-    """
-        Executes bot command if the command is known
-    """
-    # Default response is help text for the user
-    default_response = "Not sure what you mean. Try 'oncall'"
+def poll_pagerduty(**payload):
+    global cfg
+    global oncall
+    global state
+    global slack_web_client
+    global event_loop
+    print("poll_pagerduty got called")
+    event_loop = asyncio.get_event_loop()
+    event_loop.call_later(60, poll_pagerduty)
+    get_oncall()
+    if (oncall['email'] != state['current_oncall']):
+        print("Oncall changed from %s to %s" % (state["current_oncall"], oncall["email"]))
+        post_current_oncall(slack_web_client, cfg["oncall_announce_channel"], "The current oncall network engineer is now:")
+        state['current_oncall'] = oncall['email']
+        with open('state.yml', 'w') as outfile:
+            yaml.safe_dump(state, outfile, default_flow_style=False)
 
-    # Finds and executes the given command, filling in response
-    response = None
-    # Implement commands here!
-    if command.startswith("oncall"):
-        post_current_oncall(channel)
-        return
+@slack.RTMClient.run_on(event="hello")
+def rtm_init(**payload):
+    """grab some stuff from RTMClient when it starts so our scheduled actions
+       can make use of it.
+    """
+    global slack_web_client
+    global event_loop
+    global broadcast_channel
+    global polling_initialized
+    print("rtm_init got run")
+    slack_web_client = payload["web_client"]
+    if not polling_initialized:
+        event_loop.call_soon(poll_pagerduty)
+        polling_initialized = 1
+    #print("calling conversations_info")
+    #channel_info = slack_web_client.conversations_info(channel = cfg['oncall_announce_channel'])
+    #print("conversations_info called")
+    #print(json.dumps(channel_info))
+    #if not channel_info["ok"]:
+    #    print("Looking up channel: %s" % channel)
+    #    print("Got an error from conversations.info: %s" % channel_info["error"])
 
-    # Sends the response back to the channel if the command returned one
-    # instead of exiting
-    slack_client.api_call(
-        "chat.postMessage",
-        channel=channel,
-        text=response or default_response
-    )
+@slack.RTMClient.run_on(event="message")
+def message(**payload):
+    """process channel messages
+    """
+    print("message received");
+    data = payload["data"]
+    web_client = payload["web_client"]
+    channel_id = data.get("channel")
+    user_id = data.get("user")
+    text = data.get("text")
+
+    if text and text.lower() == PREFIX_CHAR + "oncall":
+        return post_current_oncall(web_client, channel_id)
 
 if __name__ == "__main__":
-    while True:
-        try:
-            print("Connecting to Slack...")
-            connected = slack_client.rtm_connect(with_team_state=False, auto_reconnect=True)
-            if connected:
-                print("Netops Bot connected and running!")
-                # Read bot's user ID by calling Web API method `auth.test`
-                bot_id = slack_client.api_call("auth.test")["user_id"]
-                last_oncall_check = 0
+    global cfg
+    global oncall
+    global state
+    global event_loop
+    with open("config.yml", 'r') as ymlfile:
+        cfg = yaml.safe_load(ymlfile)
+    oncall = cfg["default_oncall"]
+    ssl_context = ssl_lib.create_default_context(cafile=certifi.where())
+    state = { "current_oncall": "nobody" }
+    try:
+        with open("state.yml", 'r') as ymlfile:
+            try:
+                state = yaml.safe_load(ymlfile)
+            except:
                 state = { "current_oncall": "nobody" }
-                get_channels()
-                try:
-                    with open("state.yml", 'r') as ymlfile:
-                        try:
-                            state = yaml.safe_load(ymlfile)
-                        except:
-                            state = { "current_oncall": "nobody" }
-                            pass
-                except IOError:
-                    pass
-                while True:
-                    ts = time.time()
-                    if (ts - last_oncall_check) > 60:
-                        get_oncall()
-                        last_oncall_check = ts
-                        if (oncall["email"] != state['current_oncall']):
-                            print("Oncall changed from %s to %s" % (state["current_oncall"], oncall["email"]))
-                            post_current_oncall(channels[DEFAULT_CHANNEL]['id'],"The current oncall network engineer is now:")
-                            state['current_oncall'] = oncall["email"]
-                            with open('state.yml', 'w') as outfile:
-                                yaml.safe_dump(state, outfile, default_flow_style=False)
-                    command, channel = parse_bot_commands(slack_client.rtm_read())
-                    if command:
-                        handle_command(command, channel)
-                    time.sleep(RTM_READ_DELAY)
-            else:
-                print("Connection failed.")
-                print("Making another attempt in %d seconds..." % RECONNECT_DELAY)
-                time.sleep(RECONNECT_DELAY)
-        except requests.exceptions.ConnectionError as e:
-            print("Connection disconnected: %s" % e)
-            print("Re-connecting in %d seconds..." % RECONNECT_DELAY)
-            time.sleep(RECONNECT_DELAY)
-            pass
-        except Exception as e:
-            print("Connection disconnected: %s" % e)
-            print("Re-connecting in %d seconds..." % RECONNECT_DELAY)
-            time.sleep(RECONNECT_DELAY)
-            pass
-            #raise
-
+                pass
+    except IOError:
+        pass
+    event_loop = asyncio.get_event_loop()
+    rtm_client = slack.RTMClient(
+        token=cfg['slack_api_token'],
+        ssl=ssl_context,
+        loop=event_loop,
+        connect_method='rtm.start'
+    )
+    print("Starting rtm_client")
+    try:
+        rtm_client.start()
+        print("")
+        print("rtm_client exited")
+    except Exception as e:
+        print("rtm_client exited with an error: %s" % e)
+        raise
